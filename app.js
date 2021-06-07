d54b7b1328ccb7f70add4770d24d2abdbd2e9bdc
Tochukwu Idoko <idokotheodore@gmail.com>
2021-06-07 19:29:52 +0200

    first-stage

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let cookieParser = require('cookie-parser');
+const fs = require('fs');
+const cors = require('cors')
 const expressValidator = require('express-validator');
 const dotenv = require('dotenv');
 
@@ -26,12 +28,25 @@ mongoose
 const postRoutes = require('./routes/post');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+// apiDocs
+app.get('/', (req, res) => {
+  fs.readFile('docs/apiDocs.json', (err, data) => {
+    if(err) {
+      res.status(400).json({
+        error: err
+      })
+    }
+    const docs = JSON.parse(data)
+    res.json(docs)
+  })
+} )
 
 // middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(expressValidator());
+app.use(cors())
 app.use('/', postRoutes);
 app.use('/', authRoutes);
 app.use('/', userRoutes);
