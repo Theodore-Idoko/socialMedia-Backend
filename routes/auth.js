77da36992310951ab491e99662537ce7ef52e1b4
Tochukwu Idoko <idokotheodore@gmail.com>
2021-06-24 17:51:37 +0200

    socialLogin

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const { signup, signin, signout }= require('../controllers/auth');
-const { userById }= require('../controllers/user');
-const { userSignupValidator} = require('../validator');
+const {
+  signup,
+  signin,
+  signout,
+  forgotPassword,
+  resetPassword,
+  socialLogin,
+} = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const { userSignupValidator, passwordResetValidator } = require('../validator');
 
 const router = express.Router();
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
+router.post("/social-login", socialLogin); 
+
+// password forgot and reset routes
+router.put('/forgot-password', forgotPassword);
+router.put('/reset-password', passwordResetValidator, resetPassword);
 
 // any route containing  : userId, our app will first execute userById()
-router.param('userId', userById)
+router.param('userId', userById);
 
 module.exports = router;
